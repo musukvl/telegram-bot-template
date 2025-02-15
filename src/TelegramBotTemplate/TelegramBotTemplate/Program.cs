7b2ae9ddc20dcb738c7668ac66f4404474df642e
Alexey Ryazhskikh <musukvl@gmail.com>
2025-02-16 00:26:56 +0200

    Init

--- src/TelegramBotTemplate/TelegramBotTemplate/Program.cs
+++ src/TelegramBotTemplate/TelegramBotTemplate/Program.cs
@@ -1,39 +1,37 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 using Telegram.Bot; 
 using TelegramBotTemplate;
 using TelegramBotTemplate.Services;
 
-using IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration((context, builder) =>
-    {
-        // Add Bot configuration
-        builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-        builder.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-        builder.AddEnvironmentVariables();
-    })
-    .ConfigureServices((context, services) =>
-    {
-        // Register Bot configuration
-        services.Configure<BotConfiguration>(context.Configuration.GetSection("BotConfiguration"));
 
-        // Register named HttpClient to benefits from IHttpClientFactory and consume it with ITelegramBotClient typed client.
-        // See https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-5.0#typed-clients
-        // and https://docs.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/use-httpclientfactory-to-implement-resilient-http-requests
-        services.AddHttpClient("telegram_bot_client").RemoveAllLoggers()
-            .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
-            {
-                BotConfiguration? botConfiguration = sp.GetService<IOptions<BotConfiguration>>()?.Value;
-                ArgumentNullException.ThrowIfNull(botConfiguration);
-                TelegramBotClientOptions options = new(botConfiguration.BotToken);
-                return new TelegramBotClient(options, httpClient);
-            });
+// use web application just to have /health endpoint for running in container environment
+var builder = WebApplication.CreateBuilder(args);
 
-        services.AddScoped<UpdateHandler>();
-        services.AddScoped<ReceiverService>();
-        services.AddHostedService<PollingBackgroundService>();
-    })
-    .Build();
-await host.RunAsync();
+// yaml instead of json just because yaml is more human-readable
+builder.Configuration.AddYamlFile("appsettings.yml", optional: false, reloadOnChange: true);
+builder.Configuration.AddEnvironmentVariables();
+builder.Configuration.AddUserSecrets<Program>();
+
+builder.Services.AddHealthChecks();
+
+builder.Services.Configure<BotConfiguration>(builder.Configuration.GetSection("BotConfiguration"));
+
+
+// Register named HttpClient to benefits from IHttpClientFactory and consume it with ITelegramBotClient typed client.
+// See https://docs.microsoft.com/en-us/aspnet/core/fundamentals/http-requests?view=aspnetcore-5.0#typed-clients
+// and https://docs.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/use-httpclientfactory-to-implement-resilient-http-requests
+builder.Services.AddHttpClient("telegram_bot_client").RemoveAllLoggers()
+    .AddTypedClient<ITelegramBotClient>((httpClient, sp) =>
+    {
+        BotConfiguration? botConfiguration = sp.GetService<IOptions<BotConfiguration>>()?.Value;
+        ArgumentNullException.ThrowIfNull(botConfiguration);
+        TelegramBotClientOptions options = new(botConfiguration.BotToken);
+        return new TelegramBotClient(options, httpClient);
+    });
+
+builder.Services.AddScoped<UpdateHandler>();
+builder.Services.AddScoped<ReceiverService>();
+builder.Services.AddHostedService<PollingBackgroundService>();
+ 
+var app = builder.Build();
+app.Run();
